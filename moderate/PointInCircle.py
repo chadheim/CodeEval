038ce3a82268294afdfb9145d899c352ffc10215
Chad Heim <chad.m.heim@gmail.com>
2014-01-31 15:52:36 -0600

    point in circle python

---
+++ moderate/PointInCircle.py
@@ -0,0 +1,11 @@
+import sys
+import math
+import re
+with open(sys.argv[1]) as f:
+    for line in f:
+        m = re.findall(r'([-+]?[0-9]*\.?[0-9]+)', line)
+        if math.hypot(float(m[3])-float(m[0]), float(m[4])-float(m[1])) <= float(m[2]):
+            print 'true'
+        else:
+            print 'false'
+
