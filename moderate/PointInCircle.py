93bf4e8e3af2ec9758cef66b5db4afaa16e277e0
Chad Heim <chad.m.heim@gmail.com>
2014-01-31 23:52:03 -0600

    py2->py3

--- moderate/PointInCircle.py
+++ moderate/PointInCircle.py
@@ -5,7 +5,7 @@ with open(sys.argv[1]) as f:
     for line in f:
         m = re.findall(r'([-+]?[0-9]*\.?[0-9]+)', line)
         if math.hypot(float(m[3])-float(m[0]), float(m[4])-float(m[1])) <= float(m[2]):
-            print 'true'
+            print('true')
         else:
-            print 'false'
+            print('false')
 
