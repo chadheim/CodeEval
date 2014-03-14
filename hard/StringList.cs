a921142c8aa9c926b312cb53a0224ab492fa805c
Chad Heim <chad.heim@zyquest.com>
2014-03-14 13:56:45 -0500

    string list in cs

--- hard/StringList.cs
+++ hard/StringList.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StringList_CSharp
+class StringList
 {
-    class StringList
+    static SortedSet<string> words = new SortedSet<string>();
+    static string letters;
+    static int wordSize;
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        using (StreamReader reader = File.OpenText(args[0]))
         {
+            while (!reader.EndOfStream)
+            {
+                string[] parts = reader.ReadLine().Split(',');
+                wordSize = Int32.Parse(parts[0]);
+                letters = parts[1];
+                words.Clear();
+
+                BuildWords("");
+
+                Console.WriteLine(string.Join(",", words));
+            }
         }
+    }
+
+    static void BuildWords(string word)
+    {
+        if (word.Length < wordSize)
+            foreach (char c in letters)
+                BuildWords(word + c);
+        else
+            words.Add(word);
     }
 }
