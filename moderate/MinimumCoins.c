6d4fd3a13477552347e41dce5cf5c9551b218f4a
Chad Heim <chad.m.heim@gmail.com>
2014-04-03 11:43:28 -0500

    Update MinimumCoins.c

--- moderate/MinimumCoins.c
+++ moderate/MinimumCoins.c
@@ -6,12 +6,14 @@ int main(int argc, char* argv[]) {
     FILE *f = fopen(argv[1], "r");
     while (fscanf(f, "%d", &n) == 1) {
 
-        int a = n / 5;
+        int c = 0;
+        c += n / 5;
         n %= 5;
-        int b = n / 3;
+        c += n / 3;
         n %= 3;
+        c += n;
 
-        printf("%d\n", a + b + n);
+        printf("%d\n", c);
     }
     fclose(f);
     return 0;
